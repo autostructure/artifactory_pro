3cd2a34a36e066e8da20960961eac516ad1769c9
Bryan Belanger <bbelanger@azcender.com>
2016-06-22 22:15:18 -0400

    Clean init

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,20 +12,18 @@
 #
 
 class artifactory_pro(
-  String $license_key      = $::artifactory_pro::params::license_key,
-  String $package_name     = $::artifactory_pro::params::package_name,
-  String $service_name     = $::artifactory_pro::params::service_name,
-  String $plugins_dir      = $::artifactory_pro::params::plugins_dir,
+  String $license_key  = $::artifactory_pro::params::license_key,
+  String $package_name = $::artifactory_pro::params::package_name,
+  String $service_name = $::artifactory_pro::params::service_name,
+  String $plugins_dir  = $::artifactory_pro::params::plugins_dir,
 ) inherits ::artifactory_pro::params {
   contain ::artifactory_pro::yum
   contain ::artifactory_pro::install
   contain ::artifactory_pro::config
   contain ::artifactory_pro::service
 
-  class { '::artifactory_pro::yum': }     ->
-  class { '::artifactory_pro::install': } ->
-  class { '::artifactory_pro::config': }  ~>
-  class { '::artifactory_pro::service': } ->
-  Class['::artifactory_pro']
-
+  Class['::artifactory_pro::yum']     ->
+  Class['::artifactory_pro::install'] ->
+  Class['::artifactory_pro::config']  ~>
+  Class['::artifactory_pro::service']
 }
