e2a4ee1133cb46e99b7690766c4e174b597db4a0
Bryan Belanger <bbelanger@azcender.com>
2016-06-27 21:54:19 -0400

    Move storage stuff

--- manifests/config.pp
+++ manifests/config.pp
@@ -26,23 +26,32 @@ class artifactory_pro::config {
     mode    => '0664',
   }
 
-  file { '/var/opt/jfrog/artifactory/etc/storage.properties':
-    ensure  => file,
-    content => epp(
-      'artifactory/oracle.properties.epp',
-      {
-        db_url                         => $::artifactory_pro::db_url,
-        db_username                    => $::artifactory_pro::db_username,
-        db_password                    => $::artifactory_pro::db_password,
-        db_type                        => $::artifactory_pro::db_type,
-        binary_provider_type           => $::artifactory_pro::binary_provider_type,
-        pool_max_active                => $::artifactory_pro::pool_max_active,
-        pool_max_idle                  => $::artifactory_pro::pool_max_idle,
-        binary_provider_cache_maxSize  => $::artifactory_pro::binary_provider_cache_maxSize,
-        binary_provider_filesystem_dir => $::artifactory_pro::binary_provider_filesystem_dir,
-        binary_provider_cache_dir      => $::artifactory_pro::binary_provider_cache_dir,
+  # Install storage.properties if Available
+  if(
+    $::artifactory_pro::db_url or
+    $::artifactory_pro::db_username or
+    $::artifactory_pro::db_password or
+    $::artifactory_pro::db_type
+    ) {
+      file { '/var/opt/jfrog/artifactory/etc/storage.properties':
+        ensure  => file,
+        content => epp(
+          'artifactory/oracle.properties.epp',
+          {
+            db_url                         => $::artifactory_pro::db_url,
+            db_username                    => $::artifactory_pro::db_username,
+            db_password                    => $::artifactory_pro::db_password,
+            db_type                        => $::artifactory_pro::db_type,
+            binary_provider_type           => $::artifactory_pro::binary_provider_type,
+            pool_max_active                => $::artifactory_pro::pool_max_active,
+            pool_max_idle                  => $::artifactory_pro::pool_max_idle,
+            binary_provider_cache_maxSize  => $::artifactory_pro::binary_provider_cache_maxSize,
+            binary_provider_filesystem_dir => $::artifactory_pro::binary_provider_filesystem_dir,
+            binary_provider_cache_dir      => $::artifactory_pro::binary_provider_cache_dir,
+          }
+        ),
+        mode    => '0664',
       }
-    ),
-    mode    => '0664',
+    }
   }
 }
