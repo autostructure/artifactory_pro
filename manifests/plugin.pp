b0ad19d6cd4f8daf955cd6e7c7e78c352af0a12d
Bryan Belanger <bbelanger@azcender.com>
2016-07-12 15:29:52 -0400

    Add plugin resource

---
+++ manifests/plugin.pp
@@ -0,0 +1,23 @@
+#
+#
+define artifactory_pro::plugin(
+  String $url,
+  )
+{
+  # Default file sould have artifactory owner and group
+  File {
+    owner => 'artifactory',
+    group => 'artifactory',
+    mode  => 'a+rx',
+  }
+
+  $file_name =  regsubst($url, '.+\/([^\/]+)$', '\1')
+
+  ::wget::fetch { $url:
+    destination => "${::artifactory::artifactory_home}/etc/plugins/${file_name}",
+  }
+
+  file { "${::artifactory::artifactory_home}/etc/plugins/${file_name}":
+    ensure => file,
+  }
+}
