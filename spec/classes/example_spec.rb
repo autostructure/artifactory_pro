28cc0465e26a90dbc382f6f8bd58ad0ef868d932
Bryan Belanger <bbelanger@azcender.com>
2016-07-22 12:24:24 -0400

    Cleanup tests.

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -11,28 +11,18 @@ describe 'artifactory_pro' do
         context "artifactory_pro class without any parameters" do
           it { is_expected.to compile.with_all_deps }
 
-          it { is_expected.to contain_class('artifactory_pro::params') }
-          it { is_expected.to contain_class('artifactory_pro::install').that_comes_before('artifactory_pro::config') }
+          #it { is_expected.to contain_class('artifactory_pro::params') }
+          it { is_expected.to contain_class('artifactory::install').that_comes_before('artifactory_pro::config') }
+          it { is_expected.to contain_class('artifactory_pro::config').that_comes_before('artifactory_pro::post_config') }
           it { is_expected.to contain_class('artifactory_pro::config') }
-          it { is_expected.to contain_class('artifactory_pro::service').that_subscribes_to('artifactory_pro::config') }
+          it { is_expected.to contain_class('artifactory_pro::post_config') }
+          it { is_expected.to contain_class('artifactory::service').that_subscribes_to('artifactory_pro::config') }
+          it { is_expected.to contain_class('artifactory::service').that_subscribes_to('artifactory_pro::post_config') }
 
-          it { is_expected.to contain_service('artifactory_pro') }
-          it { is_expected.to contain_package('artifactory_pro').with_ensure('present') }
+          it { is_expected.to contain_service('artifactory') }
+          it { is_expected.to contain_package('jfrog-artifactory-pro').with_ensure('present') }
         end
       end
-    end
-  end
-
-  context 'unsupported operating system' do
-    describe 'artifactory_pro class without any parameters on Solaris/Nexenta' do
-      let(:facts) do
-        {
-          :osfamily        => 'Solaris',
-          :operatingsystem => 'Nexenta',
-        }
-      end
-
-      it { expect { is_expected.to contain_package('artifactory_pro') }.to raise_error(Puppet::Error, /Nexenta not supported/) }
     end
   end
 end
