e026a372093859c9b62efb895c9afcc38e8468d3
Bryan Belanger <bryan@autostructure.io>
2018-08-12 11:49:12 -0400

    Updated to PDK. Cleaned up testing.

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -14,12 +14,12 @@ describe 'artifactory_pro' do
           it { is_expected.to compile.with_all_deps }
 
           # it { is_expected.to contain_class('artifactory_pro::params') }
-          it { is_expected.to contain_class('artifactory').that_comes_before('artifactory_pro::config') }
-          it { is_expected.to contain_class('artifactory_pro::config').that_comes_before('artifactory_pro::post_config') }
+          it { is_expected.to contain_class('artifactory').that_comes_before('Class[artifactory_pro::config]') }
+          it { is_expected.to contain_class('artifactory_pro::config').that_comes_before('Class[artifactory_pro::post_config]') }
           it { is_expected.to contain_class('artifactory_pro::config') }
           it { is_expected.to contain_class('artifactory_pro::post_config') }
-          it { is_expected.to contain_class('artifactory::service').that_subscribes_to('artifactory_pro::config') }
-          it { is_expected.to contain_class('artifactory::service').that_subscribes_to('artifactory_pro::post_config') }
+          it { is_expected.to contain_class('artifactory::service').that_subscribes_to('Class[artifactory_pro::config]') }
+          it { is_expected.to contain_class('artifactory::service').that_subscribes_to('Class[artifactory_pro::post_config]') }
 
           it { is_expected.to contain_service('artifactory') }
           it { is_expected.to contain_package('jfrog-artifactory-pro').with_ensure('present') }
